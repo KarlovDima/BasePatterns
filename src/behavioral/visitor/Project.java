30d2930e2a118ac24f120f89333e7ab426d2a616
Unknown <da.erofeeva@gmail.com>
2018-01-04 18:49:08 +0200

    Add Visitor pattern

---
+++ src/behavioral/visitor/Project.java
@@ -0,0 +1,19 @@
+package behavioral.visitor;
+
+public class Project implements ProjectElement {
+    private ProjectElement[] projectElements;
+
+    public Project() {
+        projectElements = new ProjectElement[]{
+                new ProjectClass(),
+                new Database(),
+                new Test()
+        };
+    }
+
+    @Override
+    public void beWritten(Developer developer) {
+        for (ProjectElement projectElement : projectElements)
+            projectElement.beWritten(developer);
+    }
+}
